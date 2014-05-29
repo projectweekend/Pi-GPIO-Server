039768aeacbf2ad3ca3d498d035f2fcf1163ff8f
Brian Hines <brian@projectweekend.net>
2014-05-29 00:44:43 -0400

    Add parser to basic resource

--- pi_gpio/meta.py
+++ pi_gpio/meta.py
@@ -1,10 +1,11 @@
-from flask.ext.restful import Resource, marshal
+from flask.ext.restful import Resource, marshal, reqparse
 
 
 class BasicResource(Resource):
 
     def __init__(self):
         super(BasicResource, self).__init__()
+        self.parser = reqparse.RequestParser()
 
     def response(self, data, code):
         return marshal(data, self.fields), code
