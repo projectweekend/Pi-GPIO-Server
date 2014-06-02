42be269d3dbf6c9cfe5b32521d3fcbb6af054123
Brian Hines <brian@projectweekend.net>
2014-06-01 21:08:17 -0500

    Moving things around

--- pi_gpio/config/pins.py
+++ pi_gpio/config/pins.py
@@ -72,32 +72,3 @@ class PinHttpManager(PinManager):
             return True
         except KeyError:
             return None
-
-
-class PinSocketManager(PinManager):
-
-    def __init__(self, socket_response):
-        super(PinSocketManager, self).__init__()
-        self.socket_response = socket_response
-        self.initialize_pins()
-
-    def initialize_pins(self):
-        for pin_num, pin_config in self.pins.items():
-            event = pin_config.get('event', None)
-            if event:
-                print("Adding event")
-                self.add_event(pin_num, event, pin_config['bounce'])
-
-    def add_event(self, num, event, bounce):
-
-        def event_callback(pin_num):
-            pin_config = self.pins[pin_num]
-            value = 0
-            if pin_config['event'] == 'RISING':
-                value = 1
-            data = self.pin_response(pin_num, pin_config['mode'], value)
-            print(data)
-            self.socket_response(pin_num, data)
-
-        edge = self.gpio.__getattribute__(event)
-        self.gpio.add_event_detect(num, edge, callback=event_callback, bouncetime=bounce)
--- pi_gpio/sockets.py
+++ pi_gpio/sockets.py
@@ -1,16 +1,40 @@
 from flask.ext.socketio import emit
 from pi_gpio import socketio
-from config.pins import PinSocketManager
+from config.pins import PinManager
 
 
-@socketio.on('test')
-def test_socket(data):
-    emit('test', {'message': "test"})
+class PinSocketManager(PinManager):
+
+    def __init__(self):
+        super(PinSocketManager, self).__init__()
+        self.initialize_pins()
+
+    def initialize_pins(self):
+        for pin_num, pin_config in self.pins.items():
+            event = pin_config.get('event', None)
+            if event:
+                print("Adding event")
+                self.add_event(pin_num, event, pin_config['bounce'])
+
+    def add_event(self, num, event, bounce):
+
+        def event_callback(pin_num):
+            pin_config = self.pins[pin_num]
+            value = 0
+            if pin_config['event'] == 'RISING':
+                value = 1
+            data = self.pin_response(pin_num, pin_config['mode'], value)
+            print(data)
+            route = "pin:{0}".format(pin_num)
+            emit(route, data)
+
+        edge = self.gpio.__getattribute__(event)
+        self.gpio.add_event_detect(num, edge, callback=event_callback, bouncetime=bounce)
+
+
+PinSocketManager()
 
 
 def pin_event_response(pin_num, data):
     route = "pin:{0}".format(pin_num)
     emit(route, data)
-
-
-SOCKET_MANAGER = PinSocketManager(pin_event_response)
