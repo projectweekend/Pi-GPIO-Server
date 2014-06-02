23b0d311d1956dd66c77c4d5aa092bdbd8fed668
Brian Hines <brian@projectweekend.net>
2014-06-01 20:08:43 -0500

    Huh?

--- pi_gpio/config/pins.py
+++ pi_gpio/config/pins.py
@@ -97,7 +97,7 @@ class PinSocketManager(PinManager):
                 value = 1
             data = self.pin_response(pin_num, pin_config['mode'], value)
             print(data)
-            with app.request_context():
+            with app.request_context(app):
                 self.socket_response(pin_num, data)
 
         edge = self.gpio.__getattribute__(event)
