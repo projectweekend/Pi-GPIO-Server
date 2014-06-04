8ef1a25f46b344b8f9e42775f9f8ef609b3de9d0
Brian Hines <brian@projectweekend.net>
2014-06-03 21:58:58 -0500

    Testing

--- pi_gpio/static/js/controllers.js
+++ pi_gpio/static/js/controllers.js
@@ -4,9 +4,11 @@
 
 angular.module('myApp.controllers', []).
   controller('AppCtrl', function ($scope, socket) {
-
+    $scope.app_test = "APP TEST";
   }).
   controller('PinsCtrl', function ($scope, socket) {
+
+    $scope.test = "TESTING!!!";
 
   }).
   controller('MyCtrl2', function ($scope) {
