cd2cce474904f6b0075f939e2023eed1ac499a40
Brian Hines <brian@projectweekend.net>
2014-06-04 06:21:25 -0500

    Get rid of this for now

--- pi_gpio/static/js/controllers.js
+++ pi_gpio/static/js/controllers.js
@@ -14,10 +14,6 @@ cModule.controller( 'PinsCtrl', function ( $scope, Pin ) {
     $scope.Pin = Pin;
     $scope.Pin.getList();
 
-    // socket.on( 'pin:list', function ( data ) {
-    //     $scope.pinsEnabled = data;
-    // } );
-
 } );
 
 
