b36194cbc12fd61c58d1ae439241de55b45e7626
Brian Hines <brian@projectweekend.net>
2014-06-03 22:26:22 -0500

    Debugging

--- pi_gpio/static/js/controllers.js
+++ pi_gpio/static/js/controllers.js
@@ -4,19 +4,17 @@
 
 angular.module('myApp.controllers', []).
   controller('AppCtrl', function ($scope, socket) {
-    $scope.app_test = "APP TEST";
-
-    alert("TEST");
 
   }).
   controller('PinsCtrl', function ($scope, socket) {
 
     $scope.test = "TESTING!!!";
 
-    socket.emit( 'pin:list' );
     socket.on( 'pin:list', function ( data ) {
         $scope.socket_data = data;
     } );
+
+    socket.emit( 'pin:list' );
 
   }).
   controller('MyCtrl2', function ($scope) {
