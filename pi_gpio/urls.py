d860f5325e814503b39fdd1224f5bd48e2f16dbd
Brian Hines <brian@projectweekend.net>
2014-06-03 21:30:35 -0500

    Moving right along

--- pi_gpio/urls.py
+++ pi_gpio/urls.py
@@ -4,12 +4,11 @@ from flask import render_template
 from handlers import PinList, PinDetail
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 api = restful.Api(app)
-
 api.add_resource(PinList, '/api/v1/pin')
 api.add_resource(PinDetail, '/api/v1/pin/<string:pin_num>')
+
+
+@app.route('*')
+def index():
+    return render_template('index.html')
