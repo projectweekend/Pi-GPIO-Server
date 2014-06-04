e71717cca2ea47e25e21446c8abd37563ef7b317
Brian Hines <brian@projectweekend.net>
2014-06-03 20:54:22 -0500

    This

--- pi_gpio_server.py
+++ pi_gpio_server.py
@@ -3,8 +3,7 @@ from pi_gpio import app, socketio
 
 HOST = app.config['HOST']
 PORT = app.config['PORT']
-DEBUG_MODE = app.config['DEBUG']
 
 
 if __name__ == '__main__':
-    socketio.run(app, host=HOST, port=PORT, debug=DEBUG_MODE)
+    socketio.run(app, host=HOST, port=PORT)
