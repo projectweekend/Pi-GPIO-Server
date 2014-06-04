e9699be0e42da8a6f18a4c5bc421e75930521e96
Brian Hines <brian@projectweekend.net>
2014-06-03 21:38:01 -0500

    Yep

--- pi_gpio/urls.py
+++ pi_gpio/urls.py
@@ -9,6 +9,7 @@ api.add_resource(PinList, '/api/v1/pin')
 api.add_resource(PinDetail, '/api/v1/pin/<string:pin_num>')
 
 
-@app.route('/')
-def index():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     return render_template('index.html')
