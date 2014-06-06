09876700a0c28abecfb9220ea3a836a42f096125
Brian Hines <brian@projectweekend.net>
2014-06-05 21:47:11 -0500

    Let's try launching in our own bg threads

---
+++ pi_gpio/events.py
@@ -0,0 +1,31 @@
+import time
+from config.pins import PinManager
+
+
+class PinEventManager(PinManager):
+
+    def __init__(self, socketio):
+        super(PinEventManager, self).__init__()
+        self.events = []
+        self.build_events(socketio)
+
+    def build_events(self, socketio):
+        for pin_num, pin_config in self.pins.items():
+            event = pin_config.get('event', None)
+            if event:
+                self.add_event(pin_num, event, pin_config['bounce'], socketio)
+
+    def add_event(self, num, event, bounce, socketio):
+
+        def event_function():
+            edge = self.gpio.__getattribute__(event)
+            while True:
+                self.gpio.wait_for_edge(num, edge)
+                data = {
+                    'num': num,
+                    'event': event
+                }
+                socketio.emit("pin:event", data)
+                time.sleep(bounce * 0.001)
+
+        self.events.append(event_function)
--- pi_gpio/urls.py
+++ pi_gpio/urls.py
@@ -1,31 +1,27 @@
-import time
 from pi_gpio import app, socketio
 from threading import Thread
 from flask.ext import restful
 from flask import render_template
 from handlers import PinList, PinDetail
+from events import PinEventManager
 
 
 api = restful.Api(app)
 api.add_resource(PinList, '/api/v1/pin')
 api.add_resource(PinDetail, '/api/v1/pin/<string:pin_num>')
+event_manager = PinEventManager(socketio)
 
-THREAD = None
 
-
-def background_thread():
-    count = 0
-    while True:
-        time.sleep(5)
-        count += 1
-        socketio.emit('test', {'data': 'Server generated event', 'count': count})
+THREADS = []
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    global THREAD
-    if THREAD is None:
-        THREAD = Thread(target=background_thread)
-        THREAD.start()
+    global THREADS
+    if len(THREADS) == 0:
+        for event_func in event_manager.events:
+            t = Thread(target=event_func)
+            t.start()
+            THREADS.append(t)
     return render_template('index.html')
