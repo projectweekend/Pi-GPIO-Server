cee7df981e812126a3f4809fc4c83bc2759ca0fa
Brian Hines <brian@projectweekend.net>
2014-06-06 06:01:37 -0500

    Let's do this the hard way

--- pi_gpio/events.py
+++ pi_gpio/events.py
@@ -6,17 +6,8 @@ class PinEventManager(PinManager):
     def __init__(self, socketio):
         super(PinEventManager, self).__init__()
         self.socketio = socketio
-        self.build_events()
 
-    def build_events(self):
-        for pin_num, pin_config in self.pins.items():
-            event = pin_config.get('event', None)
-            if event:
-                self.add_event(pin_num, event, pin_config['bounce'])
-
-    def add_event(self, num, event, bounce):
-        edge = self.gpio.__getattribute__(event)
-
+    def build_callback(self, num, event, bounce):
         def event_callback(num):
             data = {
                 'num': num,
@@ -24,6 +15,5 @@ class PinEventManager(PinManager):
             }
             print(data)
             self.socketio.emit('pin:event', data)
-
-        self.gpio.add_event_detect(num, edge, callback=event_callback, bouncetime=bounce)
-        print("Event added: {0}".format(num))
+        print("Callback built for: {0}".format(num))
+        return event_callback
--- pi_gpio/urls.py
+++ pi_gpio/urls.py
@@ -1,6 +1,7 @@
 from pi_gpio import app, socketio
 from flask.ext import restful
 from flask import render_template
+import RPi.GPIO as GPIO
 from handlers import PinList, PinDetail
 from events import PinEventManager
 
@@ -19,4 +20,11 @@ def index(path):
     global EVENT_MANAGER
     if EVENT_MANAGER is None:
         EVENT_MANAGER = PinEventManager(socketio)
+        for pin_num, pin_config in EVENT_MANAGER.pins.items():
+            bounce = pin_config['bounce']
+            event = pin_config.get('event', None)
+            if event:
+                edge = EVENT_MANAGER.gpio.__getattribute__(event)
+                callback = EVENT_MANAGER.build_callback(pin_num, event, bounce)
+                GPIO.add_event_detect(pin_num, edge, callback=callback, bouncetime=bounce)
     return render_template('index.html')
