8e72f03058649d45428d03a3dda22720a92bbad6
Brian Hines <brian@projectweekend.net>
2014-06-05 22:44:09 -0500

    Maybe if i start it in the request

--- pi_gpio/urls.py
+++ pi_gpio/urls.py
@@ -1,5 +1,4 @@
 from pi_gpio import app, socketio
-from threading import Thread
 from flask.ext import restful
 from flask import render_template
 from handlers import PinList, PinDetail
@@ -9,26 +8,15 @@ api = restful.Api(app)
 api.add_resource(PinList, '/api/v1/pin')
 api.add_resource(PinDetail, '/api/v1/pin/<string:pin_num>')
 
-
-THREAD = None
-
-
-import time
 import RPi.GPIO as GPIO
 
-def background_thread():
-    GPIO.add_event_detect(23, GPIO.RISING)
-    while True:
-        if GPIO.event_detected(23):
-            socketio.emit('pin:event', {"message":"woohoo!"})
-            time.sleep(0.5)
+
+def event_callback(pin):
+    socketio.emit('pin:event', {"message":"woohoo!"})
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    global THREAD
-    if THREAD is None:
-        THREAD = Thread(target=background_thread)
-        THREAD.start()
+    GPIO.add_event_detect(23, GPIO.RISING, callback=event_callback)
     return render_template('index.html')
