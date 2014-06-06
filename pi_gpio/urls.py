8774517714c8c8a7f7a2be9316a23497adfa9f59
Brian Hines <brian@projectweekend.net>
2014-06-05 22:59:36 -0500

    Call event manager in index route

--- pi_gpio/urls.py
+++ pi_gpio/urls.py
@@ -2,21 +2,16 @@ from pi_gpio import app, socketio
 from flask.ext import restful
 from flask import render_template
 from handlers import PinList, PinDetail
+from events import PinEventManager
 
 
 api = restful.Api(app)
 api.add_resource(PinList, '/api/v1/pin')
 api.add_resource(PinDetail, '/api/v1/pin/<string:pin_num>')
 
-import RPi.GPIO as GPIO
-
-
-def event_callback(pin):
-    socketio.emit('pin:event', {"message":"woohoo!"})
-
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    GPIO.add_event_detect(23, GPIO.RISING, callback=event_callback)
+    PinEventManager()
     return render_template('index.html')
