44904b5d59e74b56a2e7d65c9c3aa5899d904d49
Brian Hines <brian@projectweekend.net>
2014-06-06 21:00:15 -0500

    Try this again

--- pi_gpio/events.py
+++ pi_gpio/events.py
@@ -1,3 +1,4 @@
+from pi_gpio import socketio
 from config.pins import PinManager
 
 
@@ -5,3 +6,29 @@ class PinEventManager(PinManager):
 
     def __init__(self):
         super(PinEventManager, self).__init__()
+        self.socketio = socketio
+        self.edge = {
+            'RISING': self.gpio.RISING,
+            'FALLING': self.gpio.FALLING,
+            'BOTH': self.gpio.BOTH
+        }
+
+    def build_event_callback(self, num, event):
+        def event_callback(num):
+            data = {
+                'num': num,
+                'event': event
+            }
+            self.socketio.emit('pin:event', data)
+            print(data)
+        return event_callback
+
+    def register_gpio_events(self):
+        for num, config in self.pins.items():
+            event = config.get('event', None)
+            if event:
+                edge = self.edge[event]
+                bounce = config['bounce']
+                cb = self.build_event_callback(num, event)
+                self.gpio.add_event_detect(num, edge, callback=cb, bouncetime=bounce)
+        return True
--- pi_gpio/urls.py
+++ pi_gpio/urls.py
@@ -1,52 +1,22 @@
-import yaml
-from pi_gpio import app, socketio
+from pi_gpio import app
 from flask.ext import restful
 from flask import render_template
-import RPi.GPIO as GPIO
 from handlers import PinList, PinDetail
+from events import PinEventManager
 
+
+EVENT_MANAGER = None
 
 api = restful.Api(app)
 api.add_resource(PinList, '/api/v1/pin')
 api.add_resource(PinDetail, '/api/v1/pin/<string:pin_num>')
 
-PINS_YML = './config/pins.yml'
-EDGE = {
-    'RISING': GPIO.RISING,
-    'FALLING': GPIO.FALLING,
-    'BOTH': GPIO.BOTH
-}
-PINS = None
-
-
-def read_pin_config():
-    with open(PINS_YML) as file_data:
-        pins = yaml.safe_load(file_data)
-    return pins
-
-
-def build_callback(num, event):
-    def event_callback(num):
-        data = {
-            'num': num,
-            'event': event
-        }
-        socketio.emit('pin:event', data)
-        print(data)
-    return event_callback
-
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    global PINS
-    if PINS is None:
-        PINS = read_pin_config()
-        for num, config in PINS.items():
-            event = config.get('event', None)
-            if event:
-                bounce = config['bounce']
-                edge = EDGE[event]
-                callback = build_callback(num, event)
-                GPIO.add_event_detect(num, edge, callback=callback, bouncetime=bounce)
+    global EVENT_MANAGER
+    if EVENT_MANAGER is None:
+        EVENT_MANAGER = PinEventManager()
+        EVENT_MANAGER.register_gpio_events()
     return render_template('index.html')
