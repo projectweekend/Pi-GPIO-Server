453f5fbaff9a942b9f47a8493eb587d76440223b
SCF RaspberryPi User <dev+pi@seeclickfix.com>
2017-03-08 21:04:54 +0000

    Do not install Upstart
    This will brick your PI!!

--- install/install_system.py
+++ install/install_system.py
@@ -11,10 +11,10 @@ def main():
     subprocess.call(["apt-get", "-y", "install", "python-pip"])
     subprocess.call(["apt-get", "-y", "install", "avahi-daemon"])
 
-    subprocess.call("echo Yes, do as I say! | sudo apt-get -y --force-yes install upstart", shell=True)
+    #subprocess.call("echo Yes, do as I say! | sudo apt-get -y --force-yes install upstart", shell=True)
 
     # Copy Upstart script
-    subprocess.call(["cp", "./install/gpio-server.conf", "/etc/init"])
+    #subprocess.call(["cp", "./install/gpio-server.conf", "/etc/init"])
 
 
 if __name__ == '__main__':
